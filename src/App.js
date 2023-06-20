2cf18cecb690f6bec76fc822fadf091e9c551539
Ramazan <rmzncck65@gmail.com>
2023-06-20 15:59:52 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import Footer from "./components/Footer"
+import Navs from "./components/Navs"
+import About from "./pages/About"
+import Home from "./pages/Home"
+import People from "./pages/People"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import PersonDetail from "./pages/PersonDetail"
+import Login from "./pages/Login"
+import { LoginContext } from "./context/LoginContext"
+import { useState } from "react"
+import PrivateRouter from "./pages/PrivateRouter"
 
 function App() {
+  //? Local State
+  const [user, setUser] = useState({ email: "", password: "" })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <LoginContext.Provider value={{ user, setUser }}>
+      <BrowserRouter>
+        <Navs />
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="login" element={<Login />} />
+
+          <Route path="people" element={<PrivateRouter />}>
+            <Route path="" element={<People />} />
+            <Route path=":id" element={<PersonDetail />} />
+          </Route>
+
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </LoginContext.Provider>
+  )
 }
 
-export default App;
+export default App
